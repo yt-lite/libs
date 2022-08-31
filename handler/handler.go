35224a0dbdd0a21bce9befc65eff9f98a9dc7433
trong.tek <trong.vu@sotatek.com>
2022-08-31 17:25:21 +0700

    add handler lib

---
+++ handler/handler.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/yt-lite/libs/errs"
+)
+
+func WriteResponse(w http.ResponseWriter, status int, body interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	b, ok := body.(*[]byte)
+
+	if ok {
+		w.Write(*b)
+	} else {
+		w.Write([]byte(body.(string)))
+	}
+}
+
+func WriteError(w http.ResponseWriter, err errs.AppError) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(err.Code)
+	w.Write([]byte(err.Error()))
+}
