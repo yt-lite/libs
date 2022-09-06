3d51be07b9aa2e59af7215663bb3a30f4dca5ecb
trong.tek <trong.vu@sotatek.com>
2022-09-06 15:00:42 +0700

    update handler

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/yt-lite/libs/errs"
@@ -9,18 +10,31 @@ import (
 func WriteResponse(w http.ResponseWriter, status int, body interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	b, ok := body.(*[]byte)
 
-	if ok {
-		w.Write(*b)
-	} else {
-		w.Write([]byte(body.(string)))
+	resp := map[string]interface{}{
+		"status": status,
+		"data":   body,
 	}
+
+	respByte, _ := json.Marshal(resp)
+
+	w.Write(respByte)
+
 }
 
 func WriteError(w http.ResponseWriter, err *errs.AppError) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(err.Code)
-	w.Write([]byte(err.Error()))
-}
 
+	resp := map[string]interface{}{
+		"status": err.Code,
+		"data": map[string]interface{}{
+			"message": err.Message,
+		},
+	}
+
+	respByte, _ := json.Marshal(resp)
+
+	w.Write(respByte)
+
+}
